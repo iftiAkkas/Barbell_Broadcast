242e990628640f50d0dba47b7b0ecd23544a4fd9
Hasib Altaf <hasibaltaf@iut-dhaka.edu>
2025-07-20 04:19:21 +0600

    BodyPartDetailsScreen connected with the BodyPartCard

--- app/components/BodyParts.jsx
+++ app/components/BodyParts.jsx
@@ -1,19 +1,21 @@
+import { LinearGradient } from 'expo-linear-gradient';
+import { useRouter } from 'expo-router';
 import React from 'react';
 import {
-    FlatList,
-    Image,
-    StyleSheet,
-    Text,
-    TouchableOpacity,
-    View,
+  FlatList,
+  Image,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
 } from 'react-native';
 import {
-    heightPercentageToDP as hp,
-    widthPercentageToDP as wp,
+  heightPercentageToDP as hp,
+  widthPercentageToDP as wp,
 } from 'react-native-responsive-screen';
 import { bodyParts } from '../constants/index';
-
 export default function BodyParts() {
+  const router = useRouter();
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Exercises</Text>
@@ -25,20 +27,27 @@ export default function BodyParts() {
         showsVerticalScrollIndicator={false}
         contentContainerStyle={styles.listContent}
         columnWrapperStyle={styles.columnWrapper}
-        renderItem={({ item }) => <BodyPartCard item={item} />}
+        renderItem={({ item }) => <BodyPartCard router={router} item={item} />}
       />
     </View>
   );
 }
 
-const BodyPartCard = ({ item }) => {
+const BodyPartCard = ({ item, router }) => {
   return (
-    <TouchableOpacity style={styles.cardButton}>
+    <TouchableOpacity style={styles.cardButton} onPress={() => router.push({pathname: '/pages/BodyPartDetailsScreen', params: item})}>
       <Image
         source={item.image}
         resizeMode="cover"
         style={styles.cardImage}
       />
+      <LinearGradient
+        colors={['transparent', 'rgba(0,0,0,0.9)']}
+        style={styles.gradientOverlay}
+        start={{ x: 0.5, y: 0 }}
+        end={{ x: 0.5, y: 1 }}
+      />
+      <Text style={styles.cardText}>{item?.name}</Text>
     </TouchableOpacity>
   );
 };
@@ -46,7 +55,7 @@ const BodyPartCard = ({ item }) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff', // optional
+    backgroundColor: '#fff',
   },
   title: {
     fontSize: hp('3.5%'),
@@ -67,14 +76,33 @@ const styles = StyleSheet.create({
   cardButton: {
     width: wp('44%'),
     height: hp('30%'),
-    borderRadius: 12,
+    borderRadius: 35,
     overflow: 'hidden',
     backgroundColor: '#f5f5f5',
     marginHorizontal: wp('1%'),
+    position: 'relative',
   },
   cardImage: {
     width: '100%',
     height: '100%',
-    borderRadius: 12,
+    borderRadius: 35,
+  },
+  gradientOverlay: {
+    position: 'absolute',
+    bottom: 0,
+    width: wp('44%'),
+    height: hp('15%'),
+    borderBottomLeftRadius: 35,
+    borderBottomRightRadius: 35,
+  },
+  cardText: {
+    position: 'absolute',
+    bottom: hp('2%'),
+    width: '100%',
+    textAlign: 'center',
+    color: '#fff',
+    fontWeight: '600',
+    letterSpacing: 1,
+    fontSize: hp('2.3%'),
   },
 });
---
+++ app/pages/BodyPartDetailsScreen.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+export default function BodyPartDetailsScreen() {
+    return(
+        <View>
+            <Text> Body Parts Details Screen</Text>
+        </View>
+    )
+}
