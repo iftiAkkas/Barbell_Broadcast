c5458490b0f419272f537892ada8a5239506eb52
iftiAkkas <iftiakkas@iut-dhaka.edu>
2025-07-21 23:46:52 +0600

    FireStore created

--- firebase/config.js
+++ firebase/config.js
@@ -1,5 +1,6 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
   apiKey: "AIzaSyAdCcrjAOvI1d9btJWyVZvrpyPwCwEibyo",
@@ -12,4 +13,6 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
+const auth = getAuth(app);
+const db = getFirestore(app);
+export {auth, db};
